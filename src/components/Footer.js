dea4218a5fcb05e6a53814628c5d0a3c0f5d877e
AywinK <115905373+AywinK@users.noreply.github.com>
2023-07-22 09:31:35 +0100

    footer stlyling desktop/tablet done

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,34 @@
 import Nav from "./Nav";
 import Contact from "./Contact";
 import Socials from "./Socials";
+import Logo from "../assets/icons/Logo.svg"
 
-const Footer = () => {
+const Footer = (props) => {
+
+    const footerStyles = {
+        backgroundColor: "var(--secondary-black)",
+        width: "100%",
+        padding: "20px 80px",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        color: "var(--secondary-white)"
+    };
+
+    const divStyles = {
+        display: "flex",
+        alignItems: "flex-start",
+        justifyContent: "center"
+    }
+
     return (
-        <footer>
-            <Nav />
-            <Contact />
-            <Socials />
+        <footer style={footerStyles}>
+            <img src={Logo} style={{ marginRight: "25px" }} alt="Little Lemon logo" />
+            <div style={divStyles}>
+                <Nav {...props} />
+                <Contact />
+                <Socials />
+            </div>
         </footer>
     )
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,15 @@ import Nav from "./Nav";
 import Logo from "../assets/icons/Logo.svg";
 
 const Header = () => {
+
+    const stylesObj = {
+        display: "flex",
+        margin: "20px",
+        alignItems: "center"
+    }
     return (
-        <header>
-            <img src={Logo} alt="Little Lemon logo" />
+        <header style={stylesObj}>
+            <img src={Logo} style={{ marginRight: "25px" }} alt="Little Lemon logo" />
 
             <Nav />
         </header>
