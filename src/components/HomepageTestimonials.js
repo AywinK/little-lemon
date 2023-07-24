662d54f055dec3428c31ded9770da975a9e89393
AywinK <115905373+AywinK@users.noreply.github.com>
2023-07-24 14:28:55 +0100

    adds testimonials section

--- src/components/HomepageTestimonials.js
+++ src/components/HomepageTestimonials.js
@@ -1,4 +1,8 @@
 import ReviewCard from "./ReviewCard";
+import Jimmy from "../assets/images/Jimmy.png";
+import Jane from "../assets/images/Jane.png";
+import Shannon from "../assets/images/Shannon.png";
+import Lee from "../assets/images/Lee.png";
 
 const HomepageTestimonials = () => {
 
@@ -10,6 +14,7 @@ const HomepageTestimonials = () => {
                 display: "flex",
                 flexDirection: "column",
                 alignItems: "center",
+                padding: "40px 20px"
             }}
         >
             <h2
@@ -21,16 +26,16 @@ const HomepageTestimonials = () => {
                 style={{
                     display: "flex",
                     alignItems: "center",
-                    justifyContent: "evenly",
+                    justifyContent: "space-evenly",
                     flexWrap: "wrap",
                     gap: "20px",
                     margin: "5px"
                 }}
             >
-                <ReviewCard />
-                <ReviewCard />
-                <ReviewCard />
-                <ReviewCard />
+                <ReviewCard img={Jimmy} review="amazing food and amazing atmosphere" rating={5} name="Jimmy" />
+                <ReviewCard img={Jane} review="best food in town and friendly staff" rating={5} name="Jane" />
+                <ReviewCard img={Shannon} review="best value for money" rating={4} name="Shannon" />
+                <ReviewCard img={Lee} review="great specials" rating={4} name="Lee" />
             </div>
         </section>
     )
--- src/components/ReviewCard.js
+++ src/components/ReviewCard.js
@@ -1,22 +1,43 @@
 
 
-const ReviewCard = ({ rating, name, img, review }) => {
+const ReviewCard = ({ rating = 5, name = "Jane", img, review = "test" }) => {
 
     return (
         <article
             style={{
                 height: "200px",
                 width: "200px",
-                backgroundColor: "var(--secondary-white)"
+                backgroundColor: "var(--secondary-white)",
+                borderTopRightRadius: "16px",
+                borderTopLeftRadius: "16px",
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                padding: "10px",
+                gap: "20px"
             }}
         >
             <div
+                className="star"
                 style={{
-                    width: "153px",
+                    width: `${rating * 30}px`,
                     height: "30px",
-                    backgroundColor: "none"
                 }}
             ></div>
+
+            <div
+                style={{
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    gap: "20px"
+                }}
+            >
+                <img src={img} alt={name} />
+                <h3 className="cardTitle">{name}</h3>
+            </div>
+
+            <q style={{ color: "var(--secondary-black)" }}>{review}</q>
 
         </article>
     )
