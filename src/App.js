f145e7e90626f05fd5c297d98add442094d93dcc
AywinK <115905373+AywinK@users.noreply.github.com>
2023-07-26 09:30:43 +0100

    adds hero section to reservations page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import Header from './components/Header';
 import Main from './components/Main';
 import Footer from './components/Footer';
+import Reservations from './components/pages/reserve_a_table/Reservations';
 
 function App() {
   return (
     <>
-    test
+      test
       <Header />
 
-      <Main />
+      {/* <Main /> */}
+      <Reservations />
 
       <Footer isFooter={true} />
     </>
---
+++ src/components/pages/reserve_a_table/Reservations.js
@@ -0,0 +1,13 @@
+import Hero from "./Hero";
+
+
+const Reservations = () => {
+
+    return (
+        <main>
+            <Hero />
+        </main>
+    )
+}
+
+export default Reservations;
