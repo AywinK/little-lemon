0c430a8065098c18e314446addda291a69cd3030
AywinK <115905373+AywinK@users.noreply.github.com>
2023-07-27 15:54:57 +0100

    .

--- src/components/pages/reserve_a_table/Reservations.js
+++ src/components/pages/reserve_a_table/Reservations.js
@@ -19,7 +19,6 @@ const initialiseTimes = () => {
     const date = new Date();
     const timesArr = fetchAPI(date);
     return timesArr;
-    return ["17:00", "18:00", "19:00", "20:00,", "21:00", "22:00"];
 }
 
 const updateTimes = (dispatch, date) => {
