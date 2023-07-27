82df93f42571cd6fa2beea6ee8c196e3a9195510
AywinK <115905373+AywinK@users.noreply.github.com>
2023-07-27 12:09:53 +0100

    changes useState to useReducer for time select options

--- src/components/pages/reserve_a_table/Reservations.js
+++ src/components/pages/reserve_a_table/Reservations.js
@@ -1,13 +1,38 @@
 import FormSection from "./FormSection";
 import Hero from "./Hero";
-
+import { useState, useReducer } from "react";
 
 const Reservations = () => {
+
+    // const [availableTimes, setAvailableTimes] = useState(["17:00", "18:00", "19:00", "20:00,", "21:00", "22:00"]);
+
+    const UPDATE_TIMES = "UPDATE_TIMES"
+
+    const reducer = (state, action) => {
+        switch (action.type) {
+            case UPDATE_TIMES:
+                // logic to update times based on date input
+                return initialiseTimes();
+            default:
+                return state;
+        }
+    }
+
+    const initialiseTimes = () => {
+        return ["17:00", "18:00", "19:00", "20:00,", "21:00", "22:00"];
+    }
+
+
+    const [availableTimes, dispatch] = useReducer(reducer, [], initialiseTimes);
+
+    const updateTimes = (date) => {
+        dispatch({ type: UPDATE_TIMES, date: date });
+    }
 
     return (
         <main>
             <Hero />
-            <FormSection />
+            <FormSection availableTimes={availableTimes} setAvailableTimes={updateTimes} />
         </main>
     )
 }
